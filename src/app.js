d83b22c7c7d9599b73a28df33dd0e119f045c821
Alan Yeverino <lanyev@gmail.com>
2023-02-24 19:27:17 -0700

    Skeleton 1.0

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const responseHandlers = require("./utils/handleResponses");
 const db = require("./utils/database");
 const initModels = require("./models/initModels");
+const config = require("../config").api;
+
 const userRouter = require("./users/users.router");
 const authRouter = require("./auth/auth.router");
 
@@ -26,31 +28,22 @@ app.get("/", (req, res) => {
     status: 200,
     message: "Servidor inicializado correctamente",
     data: {
-      users: "http://localhost:9000/api/v1/users",
-      conversations: "http://localhost:9000/api/v1/conversations",
+      users: `${config.host}/api/v1/users`,
     },
-  });
-});
-
-app.get("/protected", passportJwt, (req, res) => {
-  res.status(200).json({
-    message: `Hola ${req.user.firstName} este mensaje solo lo puedes ver si tienes sesion iniciada! :D`,
   });
 });
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/conversations", conversationRouter);
 
-//? Esta debe ser la ultima ruta en mi app
 app.use("*", (req, res) => {
   responseHandlers.error({
     res,
     status: 404,
-    message: "URL not found, please try with http://localhost:9000/",
+    message: `URL not found, please try with ${config.host}`,
   });
 });
 
-app.listen(9000, () => {
-  console.log("Server started at port 9000");
+app.listen(config.port, () => {
+  console.log(`Server started at port ${config.port}`);
 });
